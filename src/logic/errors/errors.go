548970512f51cccd4daca6d35b440fed85f1b67f
Maximilian Müller <maxm123@techie.com>
2022-11-28 16:48:16 +0100

    Fix 'no such car' message

--- src/logic/errors/errors.go
+++ src/logic/errors/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrFleetNotFound      = errors.New("no such fleet")
-	ErrCarNotFound        = errors.New("no such fleet")
+	ErrCarNotFound        = errors.New("no such car")
 	ErrCarNotInFleet      = errors.New("car not in fleet")
 	ErrCarAlreadyInFleet  = errors.New("car already in fleet")
 	ErrFleetAlreadyExists = errors.New("fleet already exists")
